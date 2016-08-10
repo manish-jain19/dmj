8586d71ed70793a7c47263aff1c7a314411161dc
manish jain <jainmanish01995@gmail.com>
2016-08-10 15:29:08 +0530

    added function

--- mjd.c
+++ mjd.c
@@ -8,5 +8,12 @@ c=a+b;
 d=a-b;
 printf("the add of  two nos are: %d ",c);
 printf("the sub of  two nos are: %d ",d);
-
+mul(a,b);
+}
+int mul(int a,int b)
+{
+int e;
+e=a*b;
+printf("the mul of  two nos are: %d ",e);
+return e;
 }
